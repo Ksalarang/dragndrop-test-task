635d120a1b5d3062eca8828dd769548fc1261886
Diyar Taikenov <drtaikenov@gmail.com>
2025-03-04 17:54:49 +0500

    Add scrollable background

---
+++ Assets/Scripts/DragNDrop/UI/ContentFitter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace DragNDrop.UI
+{
+    public class ContentFitter : MonoBehaviour
+    {
+        [SerializeField]
+        private RectTransform _content;
+
+        [SerializeField]
+        private RectTransform _background;
+
+        private void Start()
+        {
+            _content.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, _background.rect.width);
+            Debug.Log(_background.rect);
+        }
+    }
+}
