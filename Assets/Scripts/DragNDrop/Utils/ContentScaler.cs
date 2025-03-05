25f3f38daabfe06512e22758c82bdf95828ae2cb
Diyar Taikenov <drtaikenov@gmail.com>
2025-03-05 12:25:33 +0500

    Fit background to any screen size

---
+++ Assets/Scripts/DragNDrop/Utils/ContentScaler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace DragNDrop.Utils
+{
+    public class ContentScaler : MonoBehaviour
+    {
+        [SerializeField]
+        private Camera _camera;
+
+        private void Start()
+        {
+            var topLeft = _camera.ScreenToWorldPoint(new Vector3(0, Screen.height));
+            var bottomLeft = _camera.ScreenToWorldPoint(Vector3.zero);
+            var scale = topLeft.y - bottomLeft.y;
+
+            transform.localScale = new Vector3(scale, scale, 1);
+        }
+    }
+}
